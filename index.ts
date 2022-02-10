b543cf40b198ff76a1dd9a9b18c386d6a0a391d9
Gr3g03 <71180086+Gr3g03@users.noreply.github.com>
2022-02-10 16:04:15 +0100

    first commit

---
+++ index.ts
@@ -0,0 +1,73 @@
+import express from "express";
+
+const app = express();
+const PORT = 4000;
+
+
+
+type Quotes = {
+    id: number
+    quote: string
+    author: string
+}
+
+
+const quotes: Quotes[] = [
+    {
+        id: 1,
+        quote: 'Many of life’s failures are people who did not realize how close they were to success when they gave up',
+        author: 'Thomas A. Edison',
+    },
+    {
+        id: 2,
+        quote: 'Make memes, not war!',
+        author: 'Mark Twain',
+    },
+    {
+        id: 3,
+        quote: 'Stop making quotes I never said!',
+        author: 'Albert Einstein',
+    },
+    {
+        id: 4,
+        quote: 'The purpose of our lives is to be happy.',
+        author: 'Dalai Lama',
+    },
+    {
+        id: 5,
+        quote: 'Life is what happens when you’re busy making other plans.',
+        author: 'John Lennon'
+    },
+    {
+        id: 6,
+        quote: 'Get busy living or get busy dying.',
+        author: 'Stephen King'
+    },
+    {
+        id: 7,
+        quote: 'You only live once, but if you do it right, once is enough.',
+        author: 'Mae West'
+    },
+    {
+        id: 8,
+        quote: 'Never let the fear of striking out keep you from playing the game',
+        author: 'Babe Ruth'
+    }
+
+
+];
+
+const cors = require('cors');
+app.use(cors({
+    origin: '*'
+}));
+
+app.get('/quotes', (req, res) => {
+    res.send(quotes)
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is up and running on: http://localhost:${PORT}/quotes `);
+})
+
