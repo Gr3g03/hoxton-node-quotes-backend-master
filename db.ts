d1ff04167a58f29adbed108d9c6f3c6977970253
Gr3g03 <71180086+Gr3g03@users.noreply.github.com>
2022-02-14 15:33:10 +0100

    server done

--- db.ts
+++ db.ts
@@ -1,51 +1,86 @@
-type Quotes = {
+export type Quotes = {
     id: number;
     quote: string;
     author: string;
+    age: number;
+    firstName: string;
+    lastName: string;
+    img: string;
 }
 
 
 export const quotes: Quotes[] = [
     {
-        id: 1,
+        id: Math.random(),
         quote: 'Many of lifes failures are people who did not realize how close they were to success when they gave up',
-        author: 'Thomas A. Edison',
+        author: 'Thomas  Edison',
+        age: 50,
+        firstName: 'Thomas',
+        lastName: 'Edison',
+        img: 'https://compote.slate.com/images/3a53f815-fac0-4a76-9f80-f513178caffc.jpg%22',
     },
     {
-        id: 2,
+        id: Math.random(),
         quote: 'Make memes, not war!',
         author: 'Mark',
+        age: 50,
+        firstName: 'Mark',
+        lastName: 'Mark',
+        img: 'https://www.theparisreview.org/blog/wp-content/uploads/2017/11/audiobook-mark-twain-01-lamano-studio-photography-animation-cgi-character-design-craft-illustration-post-production-1-e1510775155945.jpg',
     },
     {
-        id: 3,
+        id: Math.random(),
         quote: 'Stop making quotes I never said!',
         author: 'Albert Einstein',
+        age: 50,
+        firstName: 'Albert',
+        lastName: 'Einstein',
+        img: 'https://parade.com/wp-content/uploads/2021/08/albert-einstein-quotes.jpg',
     },
     {
-        id: 4,
+        id: Math.random(),
         quote: 'The purpose of our lives is to be happy.',
         author: 'Dalai Lama',
+        age: 50,
+        firstName: 'Dalai',
+        lastName: 'Lama',
+        img: 'https://images-cdn.9gag.com/photo/10706_700b.jpg',
     },
     {
-        id: 5,
+        id: Math.random(),
         quote: 'Life is what happens when youre busy making other plans.',
-        author: 'John Lennon'
+        author: 'John Lennon',
+        age: 50,
+        firstName: 'John',
+        lastName: 'Lennon',
+        img: 'https://www.thatericalper.com/wp-content/uploads/2015/03/John-Lennons-Funny-Faces-1966-1.jpg',
     },
     {
-        id: 6,
+        id: Math.random(),
         quote: 'Get busy living or get busy dying.',
-        author: 'Stephen King'
+        author: 'Stephen King',
+        age: 50,
+        firstName: 'Stephen',
+        lastName: 'King',
+        img: 'https://imgix.ranker.com/list_img_v2/65/2860065/original/2860065',
     },
     {
-        id: 7,
+        id: Math.random(),
         quote: 'You only live once, but if you do it right, once is enough.',
-        author: 'Mae West'
+        author: 'Mae West',
+        age: 50,
+        firstName: 'Mae',
+        lastName: 'West',
+        img: 'https://lwlies.com/wp-content/uploads/2019/08/mae-west-sex.jpg',
     },
     {
-        id: 8,
+        id: Math.random(),
         quote: 'Never let the fear of striking out keep you from playing the game',
-        author: 'Babe Ruth'
-    }
-
+        author: 'Babe Ruth',
+        age: 50,
+        firstName: 'Babe',
+        lastName: 'Ruth',
+        img: 'https://www.morningjournal.com/wp-content/uploads/2021/08/MJE-L-BABE-RUTH-BUSTER-0813-03.jpeg',
+    },
 
 ];
--- index.ts
+++ index.ts
@@ -1,18 +1,33 @@
 import express from "express";
 import { quotes } from "./db";
 import cors from 'cors';
+import { request } from "express";
+
+import { Quotes } from "./db";
 
 const app = express();
 const PORT = 4000;
+app.use(express.json());
+app.use(cors());
 
 
-app.use(cors({
-    origin: 'http://localhost:3000'
-}));
 
+// filtering by author or display all quotes
 app.get('/quotes', (req, res) => {
-    res.send(quotes)
+    const search = req.query.search;
+
+    let match = quotes
+
+    if (typeof search === 'string') {
+
+        match = match.filter((quote) => quote.author.toUpperCase().includes(search.toUpperCase())
+        );
+    }
+
+    res.send(match)
 });
+
+
 
 
 // filtering by ID Number
@@ -27,27 +42,114 @@ app.get('/quotes/:id', (req, res) => {
 });
 
 
-// filtering by author
-app.get('/quotes/:authorId', (req, res) => {
-    const authorId = Number(req.params.authorId);
-    const match = quotes.find((quote) => quote.id === authorId);
-    if (match) {
-        res.send(match);
+
+
+app.post('/quotes', (req, res) => {
+    const firstName = req.body.firstName;
+    const img = req.body.img;
+    const lastName = req.body.lastName;
+    const quote = req.body.quote;
+    const author = req.body.author;
+    const age = req.body.age;
+
+
+    const errors = [];
+
+    if (typeof firstName !== 'string') {
+        errors.push('Name missing or wrong data format')
+    }
+    if (typeof lastName !== 'string') {
+        errors.push('lastname missing or wrong data format')
+    }
+    if (typeof author !== 'string') {
+        errors.push('author missing or wrong data format')
+    }
+    if (typeof img !== 'string') {
+        errors.push('image missing or wrong data format')
+    }
+    if (typeof quote !== 'string') {
+        errors.push('quote missing or wrong data format')
+    }
+
+
+    if (errors.length === 0) {
+        // all good here
+        const newQuote: Quotes = {
+            id: Math.random(),
+            firstName: firstName,
+            img: img,
+            lastName: lastName,
+            quote: quote,
+            author: author,
+            age: age
+
+
+
+        };
+
+        // add dog to our dogs array
+        // (like a memory db, this is forgotten when node restarts)
+        quotes.push(newQuote);
+        res.status(201).send(newQuote);
     } else {
-        res.status(404).send({ error: 'author not found.' });
+        // bad stuff happened
+        res.status(400).send({ errors: errors });
     }
 });
 
 
-
-
 // pushing data to quotes
-app.post('quotes', (req, res) => {
-    const incomingQuotes = req.body
+// app.post('quotes', (req, res) => {
+//     const lastQuoteId = Math.max(...quotes.map((nQuote => nQuote.id)))
+//     const newId = lastQuoteId + 1
 
-    quotes.push(incomingQuotes)
-    res.json(quotes)
-})
+//     const firstName = req.body.firstName;
+//     const lastName = req.body.lastName;
+//     const quote = req.body.quote;
+//     const img = req.body.img;
+//     const author = req.body.author;
+//     const age = req.body.age;
+
+
+//     const errors = [];
+
+//     if (typeof firstName !== 'string') {
+//         errors.push('Name missing or wrong data format')
+//     }
+//     if (typeof lastName !== 'string') {
+//         errors.push('lastname missing or wrong data format')
+//     }
+//     if (typeof author !== 'string') {
+//         errors.push('author missing or wrong data format')
+//     }
+//     if (typeof img !== 'string') {
+//         errors.push('image missing or wrong data format')
+//     }
+//     if (typeof quote !== 'string') {
+//         errors.push('quote missing or wrong data format')
+//     }
+
+//     if (typeof age !== 'string') {
+//         errors.push('age missing or wrong data format')
+//     }
+
+//     if (errors.length === 0) {
+//         const newQuote: Quotes = {
+//             id: newId,
+//             quote: quote,
+//             author: author,
+//             age: age,
+//             firstName: firstName,
+//             lastName: lastName,
+//             img: img,
+//         };
+
+//         quotes.push(newQuote)
+//         res.status(201).send(newQuote)
+//     } else {
+//         res.status(400).send({ errors: errors })
+//     }
+// });
 
 
 app.listen(PORT, () => {
